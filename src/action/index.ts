bedc4b2e74d24a5ea17f71fe4e9607ccb28f26e8
sjndowair <wodnjsdl1125@naver.com>
2025-05-09 17:53:04 +0900

    fix: type 강제를 해제하여 타입 보완 증가

--- src/action/index.ts
+++ src/action/index.ts
@@ -4,10 +4,17 @@ import { sendBookReviewData } from "@/api/getBookData";
 
 export const createReviewAction = async (formData: FormData) => {
   
-    const bookId = formData.get("bookId")?.toString() as string
-    const content = formData.get("content")?.toString() as string;
-    const author = formData.get("author")?.toString() as string
+    try{
+        const bookId = formData.get("bookId")?.toString() ;
+    const content = formData.get("content")?.toString() ;
+    const author = formData.get("author")?.toString();
     if(!bookId || !content || !author )  return 
     console.log(content, author)
     await sendBookReviewData({bookId, content, author});
+    }catch(err){
+        console.error(err)
+        return;
+    }
+
+    
 }
--- src/app/book/review/index.tsx
+++ src/app/book/review/index.tsx
@@ -6,10 +6,9 @@ const Review = ({bookId}: {
 }) => {
    
 
-
     return (<div>
         <form action={createReviewAction}>
-            <input hidden name={`${bookId}`} />
+            <input readOnly  name="bookId" value={`${bookId}`} hidden />
             <input required type="text" name="content" placeholder="리뷰를 입력해주세요"/>
             <input required type="text" name="author" placeholder="작성자를 입력해주세요"/>
             <button  type={`submit`}>클릭 </button>
