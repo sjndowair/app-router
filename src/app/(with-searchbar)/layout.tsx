4397018a906a1a2199fbc802d2bb3266ceb9504e
sjndowair <wodnjsdl1125@naver.com>
2025-05-10 18:11:43 +0900

    스타일 추가

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from "react";
-import Searchbar from "../../components/searchbar";
+import { ReactNode, Suspense } from "react";
+import Searchbar from "../../components/searchbar"
+import BookSkeleton from "../components/load/book-skeleton";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-        <Searchbar />
+      <Suspense fallback={<BookSkeleton />}>
+        <Searchbar /> 
         {children}
+        </Suspense>
     </div>
   );
 }
--- src/app/book/reviewList/index.tsx
+++ src/app/book/reviewList/index.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import style from "./style.module.css"
+import { useEffect , useState} from "react"
 import {IGetBookReviewDataProps} from "@/types"
 
 
 const ReviewList =  ({isBookReviewData }: {
         isBookReviewData: IGetBookReviewDataProps[]
 }) => {
+
+    const [isCreateAt, setIsCreateAt] = useState(isBookReviewData[0]?.createAt);
+    console.log(new Date(isCreateAt).toDateString() || "날짜없음")
+     
+    
     
     if(!isBookReviewData || isBookReviewData.length === 0) {
         return (<div>아직 리뷰가 존재하지않습니다. <br /> 리뷰를 작성해주세요</div>)
@@ -14,15 +22,17 @@ const ReviewList =  ({isBookReviewData }: {
 
     
     return (
-        <div>
-            <h3>리뷰 목록</h3>
+        <div className={style.reviewContainer}>
+            <h3 className={style.reviewTitle}>리뷰 목록</h3>
            {
                isBookReviewData.map((review, key) => {
                 return(
-                    <div key={review?.id} >
+                    <div className={style.reviewBox} key={review?.id} >
                         <h4>{review?.author}</h4>
-                        <p>{review?.createAt}</p>
-                      <p>{review.content}</p>       
+                        <p>{review.content}</p>       
+                        <br />
+                        <p className={style.reviewDate}>{new Date(review?.createAt).toLocaleString()}</p>
+                
                 </div>
                 )
                
