0595ffdc0ee554f05d14dd21ff0356af32a484b5
sjndowair <wodnjsdl1125@naver.com>
2025-05-14 15:26:37 +0900

    fix: parllel route 처리 완료

---
+++ src/app/parllel/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react"
+import Link from "next/link"
+
+
+const Layout = ({children, sidebar, feed}: {
+    children: ReactNode,
+    feed: ReactNode,
+    sidebar: ReactNode
+}) => {
+    return (
+        <>
+        <div>
+            <Link href={`/parllel`}>parllel</Link>
+            <br />
+        <Link href={`/parllel/setting`}>Settings</Link>
+        </div>
+            <div>{sidebar}</div>
+            <div>{feed}</div>
+            <div>{children}</div>
+        </>
+    )
+}
+
+export default Layout;
