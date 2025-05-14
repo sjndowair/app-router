b9e4cf4e9e7cfac30a5a401de0670f63c94174fb
sjndowair <wodnjsdl1125@naver.com>
2025-05-14 11:09:08 +0900

    패럴렐 라우트 시험용

---
+++ src/app/parallel/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+const Layout = (children: ReactNode) => {
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default Layout;
---
+++ src/app/parallel/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+    return (<div>parallel</div>)
+}
+
+export default Page
